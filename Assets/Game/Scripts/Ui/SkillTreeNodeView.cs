c2b8a62de8d78487309b4e2caba745b3fd3f9056
Kirill Losev <11328848+kirlosev@users.noreply.github.com>
2026-02-06 06:04:28 +0400

    Show id on node icon

--- Assets/Game/Scripts/Ui/SkillTreeNodeView.cs
+++ Assets/Game/Scripts/Ui/SkillTreeNodeView.cs
@@ -1,15 +1,19 @@
 using StbData;
+using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Ui {
 public class SkillTreeNodeView : MonoBehaviour, IPointerClickHandler {
+    [SerializeField] private TMP_Text _idText;
+
     public SkillTreeNodeData Data { get; private set; }
 
     public Vector2 Pos => ((RectTransform)transform).anchoredPosition;
 
     public void Setup(SkillTreeNodeData data) {
         Data = data;
+        _idText.SetText(data.Id.ToString());
     }
 
     public void OnPointerClick(PointerEventData eventData) {
